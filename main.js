c7e22a012acf597e28be2b31b2dca2e9943f67fe
CaroGomezCala <lcgk.021@gmail.com>
2022-11-25 16:51:11 -0500

    ultimoPresentacionJs

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 addEventListener(`DOMContentLoaded`, (e)=>{
-    //console.log(`probando mensaje de consola`);
-    //alert(`visualizando alerta del navegador`);
     //prompt(`Ingresar su nombre:`,"Ejemplo `pepito perez`");
     confirm(`Deseas leer la informacion sobre Leidy Gomez?`);
     document.querySelector("body").innerHTML = "<b>Nombre: </b><em>Leidy Carolina Gomez Cala</em><br><b>Edad: </b><em>30</em><br><b>Pasatiempos: </b><em>Me gusta practicar patinaje en linea ocasionalmente; Soy fan de la ciencia ficcion, me encanta leer en mis ratos libres sobre novelas de ciencia ficcion, misterio, thrillers psicologicos. Me gusta salir a caminar cuando tengo ratos libres ya que mi trabajo es un poco estresante.</em><br><b>Que espero del curso: </b><em>Me gustaria familiarizarme mas con el lenguaje, aprender cosas nuevas que me puedan ayudar en mi desarrollo profesional</em><br>";
+    alert(`al finalizar inspecciona la pagina para ver mas informacion`);
+    console.log(`Me gusta escuchar de todo tipo de musica, pero prefiero la musica alternativa, con sonidos acusticos y letras con significados`);
+    console.log(`Uno de mis libros favoritos es la trilogia de Los Tres Cuerpos de Cixin Liu, una historia muy recomendable`);
+    console.log(`Me gustaria hacer una especializacion en un futuro, por lo que tengo el proposito de ahorrar para poder realizarlo`);
 })
