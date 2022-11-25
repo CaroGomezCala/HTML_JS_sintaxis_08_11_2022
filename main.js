819938532a50ff5bf20d7f041677138903ff23c1
CaroGomezCala <lcgk.021@gmail.com>
2022-11-25 16:32:54 -0500

    presentacion JS

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ addEventListener(`DOMContentLoaded`, (e)=>{
     //console.log(`probando mensaje de consola`);
     //alert(`visualizando alerta del navegador`);
     //prompt(`Ingresar su nombre:`,"Ejemplo `pepito perez`");
-    //confirm(`Estas entendiendo JS`);
+    confirm(`Deseas leer la informacion sobre Leidy Gomez?`);
     document.querySelector("body").innerHTML = "<b>Nombre: </b><em>Leidy Carolina Gomez Cala</em><br><b>Edad: </b><em>30</em><br><b>Pasatiempos: </b><em>Me gusta practicar patinaje en linea ocasionalmente; Soy fan de la ciencia ficcion, me encanta leer en mis ratos libres sobre novelas de ciencia ficcion, misterio, thrillers psicologicos. Me gusta salir a caminar cuando tengo ratos libres ya que mi trabajo es un poco estresante.</em><br><b>Que espero del curso: </b><em>Me gustaria familiarizarme mas con el lenguaje, aprender cosas nuevas que me puedan ayudar en mi desarrollo profesional</em><br>";
 })
